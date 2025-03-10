253903b92f32a9cb19505f63cf32e31de5205321
Ani <a.gvin3@gmail.com>
2025-03-10 12:38:51 +0400

    update .env

--- auth/auth.router.js
+++ auth/auth.router.js
@@ -4,48 +4,49 @@ const jwt = require("jsonwebtoken");
 const usersModel = require("../models/users.model");
 const isAuth = require("../middlewares/isAuth.middleaware");
 
-
 require("dotenv").config();
 
 const authRouter = Router();
 
+authRouter.post("/sign-up", async (req, res) => {
+  const { fullName, email, password } = req.body;
+  if (!fullName || !email || !password)
+    return res.status(400).json({ message: "wrong params" });
 
+  const existUser = await usersModel.findOne({ email });
+  if (existUser)
+    return res.status(400).json({ message: "user already exists" });
 
-authRouter.post('/sign-up', async (req, res) =>{
-    const {fullName, email, password} = req.body
-    if(!fullName || !email || !password) return res.status(400).json({message: "wrong params"})
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    const existUser = await usersModel.findOne({email})
-    if(existUser) return res.status(400).json({message: "user already exists"})
+  await usersModel.create({ fullName, email, password: hashedPassword });
+  res.status(201).json({ message: "user created successfully" });
+});
+authRouter.post("/sign-in", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    return res.status(400).json({ message: "email or password is required" });
 
-    const hashedPassword = await bcrypt.hash(password, 10)
+  const existUser = await usersModel.findOne({ email });
+  if (!existUser)
+    return res.status(400).json({ message: "email or password is incorect" });
 
-    await usersModel.create({fullName, email, password: hashedPassword})
-    res.status(201).json({message: "user created successfully"})
-})
-authRouter.post('/sign-in', async (req, res) => {
-    const {email, password} = req.body
-    if(!email || !password) return res.status(400).json({message: "email or password is required"})
+  const isPasswordEqual = await bcrypt.compare(password, existUser.password);
+  if (!isPasswordEqual)
+    return res.status(400).json({ message: "email or password is incorect" });
 
-    const existUser = await usersModel.findOne({email})
-    if(!existUser) return res.status(400).json({message: "email or password is incorect"})
+  const payLoad = {
+    userId: existUser._id,
+  };
 
-    const isPasswordEqual = await bcrypt.compare(password, existUser.password)
-    if(!isPasswordEqual) return res.status(400).json({message: "email or password is incorect"})
+  const token = jwt.sign(payLoad, process.env.JWT_SECRET, { expiresIn: "1h" });
 
-    const payLoad = {
-        userId: existUser._id
-    }
+  res.json({ token });
+});
 
-    const token = jwt.sign(payLoad, process.env.JWT_SECRET, {expiresIn: '1h'})
-    res.json({token})
-})
-
-
-authRouter.get('/current-user', isAuth ,async (req, res) => {
-    const user = await usersModel.findById(req.userId)
-    res.json(user)
-})
-
+authRouter.get("/current-user", isAuth, async (req, res) => {
+  const user = await usersModel.findById(req.userId);
+  res.json(user);
+});
 
 module.exports = authRouter;
